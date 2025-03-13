e0692b9e12cee0a98cb12f2ff01bc0eb160c57dc
Resources64Hub <160801482+Resources64Hub@users.noreply.github.com>
2025-03-13 21:42:14 +0300

    Create Testing_Animation_Module.lua

---
+++ Testing_Animation_Module.lua
@@ -0,0 +1,656 @@
+
+local root = {
+	ID = 0;
+	Type = "KeyframeSequence";
+	Properties = {
+		Name = "Untitled";
+	};
+	Children = {
+		{
+			ID = 1;
+			Type = "Keyframe";
+			Properties = {};
+			Children = {
+				{
+					ID = 2;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 3;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 4;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 5;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 6;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,0,0,0.969235897064209,-0.24140115082263947,-0.04803397133946419,0.24613365530967712,0.9506000280380249,0.18915027379989624,0,-0.19515401124954224,0.9807726144790649);
+									};
+									Children = {};
+								};
+								{
+									ID = 7;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+									};
+									Children = {};
+								};
+								{
+									ID = 8;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 9;
+			Type = "Keyframe";
+			Properties = {
+				Time = 0.20000000298023224;
+			};
+			Children = {
+				{
+					ID = 10;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 11;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 12;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 13;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 14;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,0,0,0.8831075429916382,-0.4597962498664856,-0.09331869333982468,0.46683233976364136,0.8413166403770447,0.2724953889846802,-0.04678179696202278,-0.28420692682266235,0.9576209783554077);
+									};
+									Children = {};
+								};
+								{
+									ID = 15;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,1,0,0,0,1);
+									};
+									Children = {};
+								};
+								{
+									ID = 16;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,1,0,0,0,1);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 17;
+			Type = "Keyframe";
+			Properties = {
+				Time = 0.38333332538604736;
+			};
+			Children = {
+				{
+					ID = 18;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 19;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 20;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 21;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 22;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,0,0,0.7708678841590881,-0.6187499165534973,-0.1513642966747284,0.6284938454627991,0.7001036405563354,0.3388955593109131,-0.10372090339660645,-0.35637524724006653,0.9285681247711182);
+									};
+									Children = {};
+								};
+								{
+									ID = 23;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,0.9969121813774109,-0.07852424681186676,0,0.07852424681186676,0.9969121813774109);
+									};
+									Children = {};
+								};
+								{
+									ID = 24;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,0.9969121813774109,0.07852424681186676,0,-0.07852424681186676,0.9969121813774109);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 25;
+			Type = "Keyframe";
+			Properties = {
+				Time = 0.5833333134651184;
+			};
+			Children = {
+				{
+					ID = 26;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 27;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 28;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 29;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 30;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,0,0,0.6129594445228577,-0.7428194284439087,-0.2692580819129944,0.7571049928665161,0.4547206163406372,0.46906405687332153,-0.22599270939826965,-0.4913738965988159,0.8411177396774292);
+									};
+									Children = {};
+								};
+								{
+									ID = 31;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,0.06518557667732239,-5.960464477539063e-08,1,0,0,0,0.9901331663131714,-0.14012902975082397,0,0.14012902975082397,0.9901331663131714);
+									};
+									Children = {};
+								};
+								{
+									ID = 32;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,0.06518560647964478,5.960464477539063e-08,1,0,0,0,0.9876794815063477,0.15649035573005676,0,-0.15649035573005676,0.9876794815063477);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 33;
+			Type = "Keyframe";
+			Properties = {
+				Time = 0.7666666507720947;
+			};
+			Children = {
+				{
+					ID = 34;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 35;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 36;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 37;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 38;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,0,0,0.82029128074646,-0.37516090273857117,-0.4317135810852051,0.5711697936058044,0.576644778251648,0.5841625928878784,0.02979046106338501,-0.7257652282714844,0.6872972249984741);
+									};
+									Children = {};
+								};
+								{
+									ID = 39;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,0.06518560647964478,-2.9802322387695312e-08,1,0,0,0,0.9940547943115234,-0.10888016223907471,0,0.10888016223907471,0.9940547943115234);
+									};
+									Children = {};
+								};
+								{
+									ID = 40;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,0.9925708174705505,0.12166813760995865,0,-0.12166813760995865,0.9925708174705505);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 41;
+			Type = "Keyframe";
+			Properties = {
+				Time = 0.9833333492279053;
+			};
+			Children = {
+				{
+					ID = 42;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 43;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 44;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 45;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 46;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,0,0,0.898654580116272,-0.07773911952972412,-0.4317135810852051,0.34421634674072266,0.7350300550460815,0.5841625928878784,0.2719101905822754,-0.6735633015632629,0.6872972249984741);
+									};
+									Children = {};
+								};
+								{
+									ID = 47;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,0.065185546875,-2.9802322387695312e-08,1,0,0,0,0.9988765120506287,-0.04738776385784149,0,0.04738776385784149,0.9988765120506287);
+									};
+									Children = {};
+								};
+								{
+									ID = 48;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,0.9988982081413269,0.046929873526096344,0,-0.046929873526096344,0.9988982081413269);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 49;
+			Type = "Keyframe";
+			Properties = {
+				Time = 1.2000000476837158;
+			};
+			Children = {
+				{
+					ID = 50;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 51;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 52;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 53;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 54;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,-1.4901161193847656e-08,0,0.8017135858535767,0.31565746665000916,-0.5075585842132568,-0.06043332815170288,0.8876314759254456,0.4565722644329071,0.5946455001831055,-0.335366815328598,0.7307024598121643);
+									};
+									Children = {};
+								};
+								{
+									ID = 55;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,0.06518557667732239,0,1,0,0,0,0.9976712465286255,0.06820552051067352,0,-0.06820552051067352,0.9976712465286255);
+									};
+									Children = {};
+								};
+								{
+									ID = 56;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,0.065185546875,-2.9802322387695312e-08,1,0,0,0,0.9956895709037781,-0.09274828433990479,0,0.09274828433990479,0.9956895709037781);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 57;
+			Type = "Keyframe";
+			Properties = {
+				Time = 1.3833333253860474;
+			};
+			Children = {
+				{
+					ID = 58;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 59;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 60;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 61;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 62;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,-7.450580596923828e-09,0,0.8017135858535767,0.19518625736236572,-0.5649404525756836,-0.06043332815170288,0.9668045043945312,0.2482679933309555,0.5946455001831055,-0.16489866375923157,0.7868960499763489);
+									};
+									Children = {};
+								};
+								{
+									ID = 63;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,-0.18633756041526794,0.01719534397125244,1,0,0,0,0.9976712465286255,0.06820552051067352,0,-0.06820552051067352,0.9976712465286255);
+									};
+									Children = {};
+								};
+								{
+									ID = 64;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,-0.185837984085083,-0.023382842540740967,1,0,0,0,0.9956895709037781,-0.09274828433990479,0,0.09274828433990479,0.9956895709037781);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+		{
+			ID = 65;
+			Type = "Keyframe";
+			Properties = {
+				Time = 1.5833333730697632;
+			};
+			Children = {
+				{
+					ID = 66;
+					Type = "Pose";
+					Properties = {
+						Name = "HumanoidRootPart";
+						Weight = 0;
+					};
+					Children = {
+						{
+							ID = 67;
+							Type = "IntValue";
+							Properties = {
+								Name = "Null";
+							};
+							Children = {};
+						};
+						{
+							ID = 68;
+							Type = "Pose";
+							Properties = {
+								Name = "Torso";
+								Weight = 0;
+							};
+							Children = {
+								{
+									ID = 69;
+									Type = "IntValue";
+									Properties = {
+										Name = "Null";
+									};
+									Children = {};
+								};
+								{
+									ID = 70;
+									Type = "Pose";
+									Properties = {
+										Name = "Head";
+										CFrame = CFrame.new(0,0,0,0.969235897064209,-0.24140115082263947,-0.04803397133946419,0.24613365530967712,0.9506000280380249,0.18915027379989624,0,-0.19515401124954224,0.9807726144790649);
+									};
+									Children = {};
+								};
+								{
+									ID = 71;
+									Type = "Pose";
+									Properties = {
+										Name = "Right Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,0.9969121813774109,-0.07852424681186676,0,0.07852424681186676,0.9969121813774109);
+									};
+									Children = {};
+								};
+								{
+									ID = 72;
+									Type = "Pose";
+									Properties = {
+										Name = "Left Arm";
+										CFrame = CFrame.new(0,0.065185546875,0,1,0,0,0,1,0,0,0,1);
+									};
+									Children = {};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+	};
+};
+
+return root
